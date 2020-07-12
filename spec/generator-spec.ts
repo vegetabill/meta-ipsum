10fc210fc6d8a2b3abd3291f7d45fa1a8625c82c
Bill D <355438+vegetabill@users.noreply.github.com>
2020-07-11 19:07:55 -0700

    add remanining unit tests

--- spec/generator-spec.ts
+++ spec/generator-spec.ts
@@ -23,12 +23,89 @@ describe("Lorem Ipsum Generator", () => {
     generator = createGenerator(vocab);
   });
 
+  const randomLength = () =>
+    chance.natural({ min: 1, max: vocab.size * 2 + 1 });
+
+  describe("paragraphs()", () => {
+    it("should produce specified number of paragraphs", () => {
+      const paragraphs = generator.paragraphs(3);
+      expect(paragraphs.length).toEqual(3);
+      paragraphs.forEach((para) => {
+        para.split(" ").forEach((word) => {
+          const normalizedWord = word.replace(".", "").toLowerCase();
+          expect(vocab.has(normalizedWord)).toBeTrue();
+        });
+      });
+    });
+  });
+
+  describe("paragraph()", () => {
+    const { min, max } = defaultOptions.paragraphSentenceRange;
+
+    it(`should produce between ${min} and ${max} sentences`, () => {
+      const paragraph = generator.paragraph();
+      const sentences = paragraph.split(". ");
+      expect(sentences.length).toBeGreaterThanOrEqual(min);
+      expect(sentences.length).toBeLessThanOrEqual(max);
+    });
+
+    it("should generate specified number of sentences", () => {
+      const sentenceCount = randomLength();
+      const paragraph = generator.paragraph(sentenceCount);
+      const sentences = paragraph.split(". ");
+      expect(sentences.length).toEqual(sentenceCount);
+    });
+  });
+
+  describe("sentence()", () => {
+    it("should produce exactly the word count specified", () => {
+      const sentence = generator.sentence(4);
+      const words = sentence.split(" ");
+      expect(words.length).toBe(4);
+    });
+
+    it("should capitalize the initial word", () => {
+      const sentence = generator.sentence();
+      const [first, ...rest] = sentence.split(" ");
+      expect(isCapitalized(first)).toBeTrue();
+    });
+
+    it("should contain all words from the vocab", () => {
+      const sentence = generator.sentence(vocab.size);
+      const words = new Set(
+        sentence
+          .replace(".", "")
+          .split(" ")
+          .map((s) => s.toLowerCase())
+      );
+      expect(words).toEqual(vocab);
+    });
+
+    const { min, max } = defaultOptions.sentenceWordRange;
+
+    it(`should produce between ${min} and ${max} words`, () => {
+      const sentence = generator.sentence();
+      const wordCount = sentence.split(" ").length;
+      expect(wordCount).toBeGreaterThanOrEqual(min);
+      expect(wordCount).toBeLessThanOrEqual(max);
+    });
+  });
+
   describe("title()", () => {
     it("should produce capitalized words", () => {
       const title = generator.title();
-      const words = title.split(/\b/);
+      const words = title.split(" ");
       words.forEach((word) => {
         expect(isCapitalized(word)).toBeTrue();
+      });
+    });
+
+    it("should produce specified length", () => {
+      const title = generator.title(vocab.size);
+      const words = title.split(" ");
+      expect(words.length).toEqual(vocab.size);
+      words.forEach((word) => {
+        expect(vocab.has(word.toLowerCase()));
       });
     });
 
